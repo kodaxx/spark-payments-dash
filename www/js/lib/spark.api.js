5bdc01ce9ddd2b57bd1b05de1579c0080bdeef8c
Spencer Kuzara <kodaxx@gmail.com>
2018-07-18 21:53:10 -0600

    init

---
+++ www/js/lib/spark.api.js
@@ -0,0 +1,89 @@
+var spark = {
+  utils: {
+
+    getExchangeRate: function(token, currency) {
+      return new Promise(resolve => {
+        spark.utils.ajax(`https://min-api.cryptocompare.com/data/generateAvg?fsym=${token}&tsym=${currency}&e=CCCAGG`)
+          .then(function(result) {
+            resolve(result.RAW.PRICE);
+          })
+          .catch(function(e) {
+            console.log(`error: ${e}`);
+          });
+      });
+    },
+
+    getAddress: function(account) {
+      return new Promise(resolve => {
+        if (account.startsWith('xpub')) {
+          console.log('xpub coming soon.')
+          swal("Sorry", "XPUB coming soon :(", "error");
+        } else {
+          resolve(account);
+        }
+      });
+    },
+
+    latestTx: function(url) {
+      return new Promise(resolve => {
+        spark.utils.ajax(url)
+          .then(function(result) {
+            console.log(result);
+            let id = '0';
+            if (result.txs.length > 0) {
+              id = result.txs[0].txid;
+            }
+            console.log(id);
+            resolve(id);
+          })
+          .catch(function(e) {
+            console.log(`error: ${e}`);
+          });
+      });
+    },
+
+    verify: function(url, initial, price) {
+      return new Promise(resolve => {
+        spark.utils.ajax(url)
+          .then(function(result) {
+            let status = false;
+            let latest = '0';
+            let found = 0;
+            let temp = 0;
+            let expected = parseFloat(price).toFixed(8);
+            if (result.txs.length > 0) {
+              latest = result.txs[0].txid;
+              temp = result.txs[0].vout[0].value;
+              if (latest != initial) {
+                found = result.txs[0].vout[0].value;
+              }
+            }
+            //app.$data.price.received = received;
+            console.log(`expected amount: ${expected}`);
+            console.log(`found amount: ${found}`);
+            console.log(`actual found: ${temp}`);
+            if (latest != initial && found >= expected && expected != 0) {
+              status = true;
+            }
+            resolve(status);
+          })
+          .catch(function(e) {
+            console.log(`error: ${e}`);
+          });
+      });
+    },
+
+    ajax: function(url) {
+      return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+          resolve(JSON.parse(this.responseText));
+        };
+        xhr.onerror = reject;
+        xhr.open('GET', url);
+        xhr.send();
+      });
+    }
+
+  }
+};
